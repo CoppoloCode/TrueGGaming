c1cbfd5a3bd1ce69f9fa751d85699e075ebdc916
CoppoloCode <matthewcoppolo@yahoo.com>
2023-03-06 14:31:15 -0500

    added files

---
+++ cancel.php
@@ -0,0 +1,3 @@
+<?php
+ header("Location: checkout.html", true, 301);
+?>
---
+++ saveProductId.js
@@ -0,0 +1,12 @@
+
+/* ------------recieves ID and saves to local storage -----------*/
+
+
+function saveProductId(productId){
+    localStorage.setItem("chosenProductId", productId);
+
+}
+
+function saveProductType(productId){
+    localStorage.setItem("chosenProductType", productId);
+}
